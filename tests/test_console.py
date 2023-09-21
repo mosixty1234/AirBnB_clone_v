79d3d403c1a48c26309b790bca01175cee63adc6
TonnyMpho <tonnympho1@gmail.com>
2023-09-21 05:32:43 +0200

    AirBnB clone - MySQL

---
+++ tests/test_console.py
@@ -0,0 +1,64 @@
+#!/usr/bin/python3
+"""Unittest module """
+import unittest
+import sys
+import io
+from console import HBNBCommand
+
+
+class TestConsole(unittest.TestCase):
+    """ unittest class for the tests"""
+    def setUp(self):
+        self.console = HBNBCommand()
+        self.stdout = sys.stdout
+        self.mock_stdout = io.StringIO()
+        sys.stdout = self.mock_stdout
+
+    def tearDown(self):
+        sys.stdout = self.stdout
+        self.mock_stdout.close()
+
+    def test_create(self):
+        """Test the 'create' command"""
+        self.console.onecmd("create User")
+        output = self.mock_stdout.getvalue().strip()
+        self.assertTrue(output.startswith("[User] ("))
+
+    def test_show(self):
+        """ Test the 'show' command"""
+        self.console.onecmd("create User")
+        obj_id = self.mock_stdout.getvalue().strip().split()[2][1:-1]
+        self.mock_stdout.truncate(0)
+        self.console.onecmd(f"show User {obj_id}")
+        output = self.mock_stdout.getvalue().strip()
+        self.assertTrue(output.startswith("[User] ("))
+
+    def test_all(self):
+        """ Test the 'all' command"""
+        self.console.onecmd("all")
+        output = self.mock_stdout.getvalue().strip()
+        self.assertIn("[User] (", output)
+
+    def test_update(self):
+        """ Test the 'update' command"""
+        self.console.onecmd("create User")
+        obj_id = self.mock_stdout.getvalue().strip().split()[2][1:-1]
+        self.mock_stdout.truncate(0)
+        self.console.onecmd(f"update User {obj_id} first_name 'John'")
+        self.console.onecmd(f"show User {obj_id}")
+        output = self.mock_stdout.getvalue().strip()
+        self.assertIn("first_name: 'John'", output)
+
+    def test_destroy(self):
+        """ Test the 'destroy' command"""
+        self.console.onecmd("create User")
+        obj_id = self.mock_stdout.getvalue().strip().split()[2][1:-1]
+        self.mock_stdout.truncate(0)
+        self.console.onecmd(f"destroy User {obj_id}")
+        self.console.onecmd(f"show User {obj_id}")
+        output = self.mock_stdout.getvalue().strip()
+        self.assertEqual(output, "** no instance found **")
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/test_models/test_engine/test_db_storage.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Unittest script """
+import unittest
+from models import storage
+from models.base_model import BaseModel
+from models.user import User
+
+
+class TestDBStorage(unittest.TestCase):
+    """ Unittest """
+    @classmethod
+    def setUpClass(cls):
+        """ Initialize the database connection and create tables"""
+        storage.reload()
+
+    def test_add_and_get(self):
+        """ Test adding and getting objects from the database"""
+        user = User(email="test@example.com", password="testpwd")
+        user.save()
+
+        retrieved_user = storage.get(User, user.id)
+        self.assertEqual(retrieved_user, user)
+
+    def test_update(self):
+        """ Test updating objects in the database"""
+        user = User(email="test@example.com", password="testpwd")
+        user.save()
+
+        user.email = "updated@example.com"
+        user.save()
+
+        retrieved_user = storage.get(User, user.id)
+        self.assertEqual(retrieved_user.email, "updated@example.com")
+
+    def test_delete(self):
+        """ Test deleting objects from the database """
+        user = User(email="test@example.com", password="testpwd")
+        user.save()
+
+        user.delete()
+
+        retrieved_user = storage.get(User, user.id)
+        self.assertIsNone(retrieved_user)
+
+
+if __name__ == '__main__':
+    unittest.main()
