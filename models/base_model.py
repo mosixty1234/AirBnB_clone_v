a9f335947dd923c4a65b1460665a6c26d899dd2f
TonnyMpho <tonnympho1@gmail.com>
2023-09-21 20:15:59 +0200

    AirBnB clone - MySQL

--- models/base_model.py
+++ models/base_model.py
@@ -14,7 +14,7 @@ class BaseModel:
     """A base class for all hbnb models"""
 
     id = Column(String(60), primary_key=True, nullable=False,
-                unique=True, default=str(uuid.uuid4()))
+                unique=True)
     created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
 
