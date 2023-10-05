3d0ae0928bda1ae0e422b4806bb35d0394b128ae
TonnyMpho <tonnympho1@gmail.com>
2023-10-05 13:32:12 -0700

    AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -1,30 +1,9 @@
 #!/usr/bin/python3
 """ Fabric script that distributes an archive to web servers"""
 from fabric.api import *
-from datetime import datetime
 from os import path
 
 env.hosts = ['100.26.178.116', '54.144.154.1']
-
-#!/usr/bin/python3
-"""
-Fabric script that generates a .tgz archive from the contents of the web_static
-"""
-from fabric.api import local
-from datetime import datetime
-
-
-def do_pack():
-    """ script that generates a .tgz archive """
-    date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    local("mkdir -p versions")
-    archive = "versions/web_static_{}.tgz".format(date_time)
-
-    if local("tar -czvf {} web_static".format(archive)).succeeded:
-        return archive
-    else:
-        return None
 
 
 def do_deploy(archive_path):
