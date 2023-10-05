b3e1565116c80be0908ee7bfd7b8d19650b52864
TonnyMpho <tonnympho1@gmail.com>
2023-10-05 14:02:42 -0700

    AirBnB clone - Deploy static

--- 2-do_deploy_web_static.py
+++ 2-do_deploy_web_static.py
@@ -7,19 +7,6 @@ from os import path
 env.hosts = ['100.26.178.116', '54.144.154.1']
 
 
-def do_pack():
-    """ script that generates a .tgz archive """
-    date_time = datetime.now().strftime("%Y%m%d%H%M%S")
-
-    local("mkdir -p versions")
-    archive = "versions/web_static_{}.tgz".format(date_time)
-
-    if local("tar -czvf {} web_static".format(archive)).succeeded:
-        return archive
-    else:
-        return None
-
-
 def do_deploy(archive_path):
     """
     function distributes an archive to web servers
@@ -30,24 +17,20 @@ def do_deploy(archive_path):
     try:
         file_name = archive_path.split('/')[-1]
         file_base = file_name.split('.')[0]
+        path_static = "/data/web_static/releases/{}/".format(file_base)
 
         put(archive_path, '/tmp/')
 
-        run("mkdir -p /data/web_static/releases/{}/".format(file_base))
+        run("mkdir -p {}".format(path_static))
 
-        run("tar -xzf /tmp/{} -C /data/web_static/releases/{}/".format(
-            file_name, file_base))
+        run("tar -xzf /tmp/{} -C {}".format(file_name, path_static))
 
         run("rm /tmp/{}".format(file_name))
-        run("""
-        mv /data/web_static/releases/{}/web_static/* 
-        /data/web_static/releases/releases/{}/""".format(file_base, file_base))
-        run("rm -rf /data/web_static/releases/{}/web_static".format(file_base))
+        run("mv {}web_static/* {}".format(path_static, path_static))
+        run("rm -rf {}web_static".format(path_static))
         run("rm -rf /data/web_static/current")
 
-        run("""
-        ln -s /data/web_static/releases/{}/ /data/web_static/current
-        """.format(file_base))
+        run("ln -s {} /data/web_static/current".format(path_static))
 
         print("New vision deployed")
 
