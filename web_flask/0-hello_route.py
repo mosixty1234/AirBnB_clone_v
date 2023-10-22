97ac5dff7bf3bdac75accd93c67ed2d1ee14eaba
TonnyMpho <tonnympho1@gmail.com>
2023-10-22 02:16:24 -0700

    AirBnB clone - Web framework

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -6,11 +6,12 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
 @app.route("/", strict_slashes=False)
-def home():
+def home_hbnb():
     """ home route """
     return "Hello HBNB!"
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
