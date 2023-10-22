88985385e6010b8a08e7e86cbabfaf07c18ca222
TonnyMpho <tonnympho1@gmail.com>
2023-10-22 04:07:07 -0700

    AirBnB clone - Web framework

---
+++ web_flask/4-number_route.py
@@ -0,0 +1,41 @@
+#!/usr/bin/python3
+""" script that starts a Flask web application:
+listening on 0.0.0.0, port 5000 Routes /: display “Hello HBNB!”
+"""
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route("/", strict_slashes=False)
+def home_hbnb():
+    """ home route """
+    return "Hello HBNB!"
+
+
+@app.route("/hbnb", strict_slashes=False)
+def hbnb():
+    """ hbnb route return HBNB"""
+    return "HBNB"
+
+
+@app.route("/c/<text>",  strict_slashes=False)
+def c_route(text):
+    """ display “C ” followed by the value of the text variable """
+    return "C {}".format(text.replace("_", " "))
+
+
+@app.route("/python", strict_slashes=False)
+@app.route("/python/<text>", strict_slashes=False)
+def python_route(text="is cool"):
+    """ display “Python ”, followed by the value of the text variable """
+    return f"Python {text.replace('_', ' ')}"
+
+
+@app.route("/number/<int:n>", strict_slashes=False)
+def number_route(n):
+    return "{} is a number".format(n)
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
