d4b7eddb71a15e766a015315016c110284d56c7d
TonnyMpho <tonnympho1@gmail.com>
2023-10-22 03:13:16 -0700

    AirBnB clone - Web framework

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -19,5 +19,11 @@ def hbnb():
     return "HBNB"
 
 
+@app.route("/c/<text>",  strict_slashes=False)
+def c_route(text):
+    """ display “C ” followed by the value of the text variable """
+    return "C {}".format(text.replace("_", " "))
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
