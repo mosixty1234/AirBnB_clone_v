9193da0e3ef9045bf53e29b5d64607fa4ab8d50f
mosixty1234 <mosixtysipho11@gmail.com>
2023-11-15 19:09:51 +0000

    msg

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -7,7 +7,7 @@ from flask import Flask
 app = Flask(__name__)
 
 
-@app.route("/", strict_slashes=False)
+@app.route("/airbnb-onepage/", strict_slashes=False)
 def home_hbnb():
     """ home route """
     return "Hello HBNB!"
